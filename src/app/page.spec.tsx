595b6c825aa9aed266b9cad0bcebe2ef679f3d26
Alan Wostenberg <awostenberg@gmail.com>
2024-06-08 00:37:17 -0600

    F1 render headline

--- src/app/page.spec.tsx
+++ src/app/page.spec.tsx
@@ -1,14 +1,31 @@
+
+import {render,screen} from '@testing-library/react';
+import Dashboard from './page';
+
+
 describe('dashboard page', () => {
 
     describe('summary', () => {
+
+        it('renders dashboard headline', () => {
+            render(<Dashboard />) ;
+
+            expect(screen.getByRole('heading').textContent).toContain("DASHBOARD")         
+
+        })
+
         it.todo('renders the first and last part of transaction id in header left');
         it.todo('renders total bitcoin holdings in header right');
         it.todo('renders live btc to usd conversion in header far right');
     
     })
 
+    describe('holdings', () => {
+        it.todo('charts USD value of holdings over time');
+    })
+    
     describe('details', () => {
-
+        
         it.todo('renders the transaction amount');
         it.todo('renders the transaction date');
         it.todo('renders the transaction confirmation status');
