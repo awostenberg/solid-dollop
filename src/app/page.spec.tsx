42ba84e211a8fecd104ecaa2e6d34c82a813326e
Alan Wostenberg <awostenberg@gmail.com>
2024-06-07 19:15:22 -0600

    t1 initial test list

---
+++ src/app/page.spec.tsx
@@ -0,0 +1,6 @@
+describe('dashboard page', () => {
+    it.todo('should render the transaction amount');
+    it.todo('should render the transaction date');
+    it.todo('should render the transaction confirmation status');
+    
+})
