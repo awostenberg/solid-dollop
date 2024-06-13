0d8b5a1244da00a4d22770066fbf254ed666bc99
Alan Wostenberg <awostenberg@gmail.com>
2024-06-13 17:17:14 -0600

    R1 dedup - replace fixed strings with mempool block time and formatter

--- src/app/components/details.tsx
+++ src/app/components/details.tsx
@@ -1,16 +1,15 @@
+import { MempoolBlocktime } from "./mempoolBlocktime";
 import { Bitcoin } from "./bitcoin";
 
-
 const sample_txns_from_figma = [
-    { 'amount': Bitcoin.fromSats(37801991), 'date': '05/01/2023', 'status': 'PENDING' },
-    { 'amount': Bitcoin.fromSats(14001991), 'date': '04/18/2023', 'status': 'PENDING' },
-    { 'amount': Bitcoin.fromSats(-38981431), 'date': '01/29/2023', 'status': 'COMPLETED' },
+    { 'amount': Bitcoin.fromSats(37801991), 'date': MempoolBlocktime.from(1682983427), 'status': 'PENDING' },
+    { 'amount': Bitcoin.fromSats(14001991), 'date': MempoolBlocktime.from(1681776000), 'status': 'PENDING' },
+    { 'amount': Bitcoin.fromSats(-38981431), 'date': MempoolBlocktime.from(1674950400), 'status': 'COMPLETED' },
 ]
 
 //todo asynchronously fetch details as I did in https://github.com/awostenberg/symmetrical-guide/blob/master/src/app/components/repos.tsx
 //and in micro test mock out with above txns as I did in https://github.com/awostenberg/symmetrical-guide/blob/master/src/app/components/repos.spec.tsx
 //this will be through the mempool api https://mempool.space/docs/api/rest#get-address-transactions
-
 
 export default function Details() {
     return (
@@ -28,7 +27,7 @@ export default function Details() {
                     <tbody>
                         {sample_txns_from_figma.map((item, index) =>
                             <tr key={index} >
-                                <td>{item.date}</td>
+                                <td>{item.date.asDisplayString()}</td>
                                 <td>{item.amount.asDisplayString()}</td>
                                 <td>{item.status}</td>
                             </tr>)
---
+++ src/app/components/mempoolBlocktime.tsx
@@ -0,0 +1,14 @@
+export class MempoolBlocktime {
+    readonly theDate: Date;
+    constructor(readonly mempool_block_time: number) { this.theDate = new Date(mempool_block_time * 1000); };;
+    static from(blocktime: number) { return new MempoolBlocktime(blocktime); };
+
+    asDisplayString(): string {
+        return this.theDate.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        });
+    };
+
+}
