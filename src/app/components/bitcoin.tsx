5fa3984b6a592e9b23238d93e44fa2c6edd90df3
Alan Wostenberg <awostenberg@gmail.com>
2024-06-14 16:16:43 -0600

    r1 fe one step declare and init readonly property

--- src/app/components/bitcoin.tsx
+++ src/app/components/bitcoin.tsx
@@ -1,7 +1,6 @@
 export class Bitcoin {
-    readonly satoshis: number;
-    constructor(readonly s: number) { this.satoshis = s; };
-    static from(satoshiAmount: number) { return new Bitcoin(satoshiAmount); };
+    constructor(readonly satoshis: number) { };
+    static from(satoshiAmount: number) { return new Bitcoin(satoshiAmount) };
     
     toDisplayString(): string { 
         return `${this.satoshis<0?'-':'+'} ${(Math.abs(this.satoshis)/100_000_000).toFixed(8)}`; }
