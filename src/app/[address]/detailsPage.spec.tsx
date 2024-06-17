05a95401f36ed6f5f4ef7272434954a7633493e8
Alan Wostenberg <awostenberg@gmail.com>
2024-06-17 10:26:34 -0600

    t1 DRY up test setup

--- src/app/[address]/detailsPage.spec.tsx
+++ src/app/[address]/detailsPage.spec.tsx
@@ -1,7 +1,9 @@
 
-// this renders the detail page for an address, using fetch mocks,
-// to avoid unreliable tests due to internet connections
-// a contract integration test will check the mock behaves faithfully. See https://martinfowler.com/bliki/ContractTest.html
+// This renders the detail page for an address, using fetch mocks,
+// to avoid unreliable tests due to internet connections and changing 3rd party (mempool) API.
+// But such loose coupling, while desirable poses a problem: how do I know my fetch mock matches the real API?
+// A contract integration test will check the mock behaves faithfully. See https://martinfowler.com/bliki/ContractTest.html
+// This is the I (for interface) in TDD guided by zombies https://blog.wingman-sw.com/tdd-guided-by-zombies
 
 import { render, screen } from '@testing-library/react';
 import DetailsPage from './page';
@@ -12,14 +14,18 @@ import fetchMock from 'jest-fetch-mock';
 fetchMock.enableMocks();
 
 describe('details page', () => {
+  let address:string;
   beforeEach(() => {
     fetchMock.resetMocks();
+
+    fetchMock.mockResponseOnce(JSON.stringify(sampleOneVout));
+    address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+    
   })
 
 
   it('renders address headline if confirmed in mempool', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(sampleOneVout));
-    const address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+
     // Async render? See https://www.marcusoft.net/2022/11/nextjs-testing-async-react-components.html
     const jsx = await DetailsPage({ params: { address: address } });
 
@@ -30,30 +36,33 @@ describe('details page', () => {
   })
 
   it('renders transaction column headers date, balance and status', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(sampleOneVout));
-    const address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+
     const jsx = await DetailsPage({ params: { address: address } });
 
     render(jsx);
 
     expect(screen.getByRole('columnheader', {name:/date/i}));
-    expect(screen.getByRole('columnheader', {name:/balance/i}));
+    expect(screen.getByRole('columnheader', {name:/balance/i})); 
    
   })
 
   it('fetches and renders balance in table', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(sampleOneVout));
-    const address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+
     const jsx = await DetailsPage({ params: { address: address } });
     render(jsx);
 
     const balance = screen.getAllByRole('cell')[1]; //first row 2nd column
+    // name 'balance' here is not entirely accurate for it's really just the vout.
+    // and while that is accurate for single use deposit-only utxos 
+    // as in grandpa bitcoin (/docs/personae/grandpa_b.md) scenario,
+    // it is not generally correct.
+    // See chapter 6 _mastering bitcoin_ "transactions" for what must be done to compute balance
+
     expect(balance.textContent).toBe('+ 0.00030236');
   })
 
   it('fetches and renders date in table', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(sampleOneVout));
-    const address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+
     const jsx = await DetailsPage({ params: { address: address } });
     render(jsx);
 
